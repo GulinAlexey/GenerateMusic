06ae314210c84224045935393140923453bf0954
GulinAlexey <gulin-2001@mail.ru>
2023-05-01 19:21:20 +0700

    Исправление метода конвертации midi format 1 to 0

--- GenerateMusic.py
+++ GenerateMusic.py
@@ -3,11 +3,12 @@ import mido
 
 format0FilenameEnd = '_format0.mid'
 
-def convert_type1_to_type0(midi_type1_file):
-    midi_type_1 = mido.MidiFile(midi_type1_file)
-    if midi_type_1.type != 0:
-        mido.merge_tracks(midi_type_1.tracks)
-    midi_type_1.save(midi_type_1.filename + format0FilenameEnd)
+def convertType1ToType0(midiType1File):
+    midiType1 = mido.MidiFile(midiType1File)
+    midiType0Tracks = mido.merge_tracks(midiType1.tracks)
+    midiType0 = mido.MidiFile(type = 0, ticks_per_beat=midiType1.ticks_per_beat)
+    midiType0.tracks.append(midiType0Tracks)
+    midiType0.save(midiType1.filename + format0FilenameEnd)
 
 layout = [
     [sg.Text('MIDI файл'), sg.InputText(),
@@ -21,4 +22,4 @@ while True:                             # The Event Loop
     if event in (None, 'Exit', 'Cancel'):
         break
     if event == 'Генерировать':
-        convert_type1_to_type0(values["inputFile"])
+        convertType1ToType0(values["inputFile"])
