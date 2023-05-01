3efc91ecd50c1b58d07a7e0b92d9dde2f9978dcd
GulinAlexey <gulin-2001@mail.ru>
2023-05-01 18:33:25 +0700

    Добавлена конвертация из midi format 1 в format 0 для более удобной обработки

--- GenerateMusic.py
+++ GenerateMusic.py
@@ -1,12 +1,24 @@
 import PySimpleGUI as sg
+import mido
+
+format0FilenameEnd = '_format0.mid'
+
+def convert_type1_to_type0(midi_type1_file):
+    midi_type_1 = mido.MidiFile(midi_type1_file)
+    if midi_type_1.type != 0:
+        mido.merge_tracks(midi_type_1.tracks)
+    midi_type_1.save(midi_type_1.filename + format0FilenameEnd)
+
 layout = [
-    [sg.Text('File'), sg.InputText(), sg.FileBrowse()],
-    [sg.Output(size=(57, 10))],
-    [sg.Submit(), sg.Cancel()]
+    [sg.Text('MIDI файл'), sg.InputText(),
+     sg.FileBrowse('Выбрать',key="inputFile",file_types=(("MIDI files", "*.mid"),))],
+    [sg.Output(size=(63, 10))],
+    [sg.Submit('Генерировать'), sg.Cancel('Отмена')]
 ]
 window = sg.Window('Generate Music', layout)
 while True:                             # The Event Loop
     event, values = window.read()
-    # print(event, values) #debug
     if event in (None, 'Exit', 'Cancel'):
         break
+    if event == 'Генерировать':
+        convert_type1_to_type0(values["inputFile"])
