213da48c197dbc656e5f1cba8602b2130ed4853d
GulinAlexey <gulin-2001@mail.ru>
2023-05-04 19:27:32 +0700

    Добавлен изменяемый список исходных файлов, добавлен popup о завершении

--- GenerateMusic.py
+++ GenerateMusic.py
@@ -1,9 +1,10 @@
 import PySimpleGUI as sg
 import mido
+import os
 
 midiFormat = '.mid'
-format0FilenameEnd = '_format_0.mid'
-resultFilenameEnd = '_result.mid'
+format0FilenameEnd = '_format_0' + midiFormat
+resultFilenameEnd = '_result' + midiFormat
 
 def convertType1ToType0(midiType1FilePath):
     midiType1 = mido.MidiFile(midiType1FilePath)
@@ -11,27 +12,45 @@ def convertType1ToType0(midiType1FilePath):
     midiType0 = mido.MidiFile(type = 0, ticks_per_beat=midiType1.ticks_per_beat)
     midiType0.tracks.append(midiType0Tracks)
     filename = midiType1FilePath.removesuffix(midiFormat) + format0FilenameEnd
+    #print(midiType0.tracks)
     midiType0.save(filename)
     return filename
 
 def MidiGenerate(midiType0FilePath):
     inputMidi = mido.MidiFile(midiType0FilePath)
-    #outputMidi = mido.MidiFile(type = 0, ticks_per_beat=inputMidi.ticks_per_beat)
-    #outputMidi.tracks.append(inputMidi)
-    outputMidi = inputMidi
+    outputMidi = mido.MidiFile(type = 0, ticks_per_beat=inputMidi.ticks_per_beat)
+    outputTrack = mido.MidiTrack()
+    outputMidi.tracks.append(outputTrack)
+    #############################
     outputMidi.save(midiType0FilePath.removesuffix(format0FilenameEnd) + resultFilenameEnd)
 
-layout = [
-    [sg.Text('MIDI файл'), sg.InputText(),
-     sg.FileBrowse('Выбрать',key="inputFile",file_types=(("MIDI files", "*.mid"),))],
-    [sg.Output(size=(63, 10))],
-    [sg.Submit('Генерировать'), sg.Cancel('Отмена')]
+midiList = []
+
+layout = [[sg.Text('Исходные MIDI-файлы:'), sg.Push(),
+    sg.Column([[sg.FileBrowse('Добавить',key="InputFile",
+    enable_events=True, file_types=(("MIDI files", "*.mid"),))]]),
+    sg.Button('Удалить', key="DeleteFile")],
+    [sg.Listbox(midiList, size=(63,10), enable_events=True,  key="MidiListView")],
+    #[sg.Text('Лог работы:')],
+    #[sg.Output(size=(63, 10))],
+    [sg.Push(), sg.Submit('Генерировать', key="Generate"),
+    sg.Cancel('Отменить и выйти', key="Cancel"), sg.Push()]
 ]
 window = sg.Window('Генерация музыки', layout, icon='app_icon.ico')
 while True:                             # The Event Loop
     event, values = window.read()
-    if event in (None, 'Exit', 'Cancel'):
+    if event in (None, 'Exit', "Cancel"):
         break
-    if event == 'Генерировать':
-        midiFileType0Path = convertType1ToType0(values["inputFile"])
-        MidiGenerate(midiFileType0Path)
+    if event == "InputFile":
+        midiList.append(values["InputFile"])
+        window["MidiListView"].update(midiList)
+    if event == "DeleteFile" and values["MidiListView"]:
+        midiList.remove(values["MidiListView"][0])
+        window["MidiListView"].update(midiList)
+    if event == "Generate":
+        for midiElement in midiList:
+            midiFileType0Path = convertType1ToType0(midiElement)
+            MidiGenerate(midiFileType0Path)
+            #os.remove(midiFileType0Path)
+        sg.popup('Успешно сгенерировано', keep_on_top=True, no_titlebar = True,
+                 any_key_closes = True, grab_anywhere = True)
