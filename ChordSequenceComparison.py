18defd8ae151934564c032438055736f794c06ea
GulinAlexey <gulin-2001@mail.ru>
2023-05-18 00:57:33 +0700

    Испр. ошибка при сравнении последовательностей аккордов

--- ChordSequenceComparison.py
+++ ChordSequenceComparison.py
@@ -3,7 +3,10 @@ from ChordComparison import chordsAreEqual
 #проверка равенства двух последовательностей (списков) аккордов
 #(абсолютное время сообщений в аккордах может отличаться)
 def chordSequencesAreEqual(chordSequence1, chordSequence2):
-    if len(chordSequence1) != len(chordSequence2):
+    try:
+        if len(chordSequence1) != len(chordSequence2):
+            return False
+    except TypeError: #если у одного из объектов нет параметра длины
         return False
     for chord1 in chordSequence1:
         if not chordsAreEqual(chord1, chordSequence2[chordSequence1.index(chord1)]):
